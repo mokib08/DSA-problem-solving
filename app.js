fe2dd692af7627674082d134adf58c077d3ad735
mokib08 <mokib08@gmail.com>
2025-04-07 01:26:05 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+
+// ## Day 42
+
+// —Complexity
+// —Time Complexity
+// —Handling for large input
+// —Complexity Representation
+// —Type and Graph
+// —Generating complexity equation
+// —TLE
+// —Space Complexity
+// —Questions
+
+
+// —Time Complexity
+// Time complexity er kono somporko nei real "time" er sathe.
+
+
+//—Handling for large input
+// kono akti company er joto employe ache tader data rakhar jonno 'system' use kora hoy jake lafge input
+
+// —Complexity Representation
+
+
+
+//1
+// variable n er input jodi NaN/string hoy tahole "Invalid input " print hobe
+// NaN check korar jonno "isNaN()" use kora hoy
+
+//2
+// variable factor er man 1 rakhte hobe, jodi 0 rakha hoy tahole 0*0=0 thakbe ar bar bar output 0 dibe
+
+//3
+// input a akti number dibe user seitar gun fol ber korake factor bolle
+
+
+//4
+// user er kache theke akti input nite hobe oi input number er "even" gula add kore print korte hobe o "odd"  gula add kore print korte hobe
+
+
+
+
+//5
+// akti number er factor ber korte bolche..mane akti number ke koto koto diye vag kora jai o remender 0 asbe ta ber korte hobe 
+// Example : 
+//  10 ==> ke 1, 2, 5, 10 diye vag kora jai o remender==0 thake 
+//  30 ==> ke 1, 2, 3, 5, 6, 10, 15, 30 diye vag kora jai .
+
+//solution
+// "nt" input er theke niya integer factors er haf number ti print korabe.. karon haf er pore r gun kora jai na sonkha ke 
+// jodi "nt" 2000 hoy tahole factor er somoy haf er pore jehetu r gun kokra jaina .. sehetu haf er porer loop onek bar cholbe ta bondho korar jonno "nt/2"
+
+
+//6
+// Prime number er defination : 2 bar divisible hoy
+// Prime number 1 er sathe divisible r nijer sathe divisible take Prime number bolle
+// only "2" ak matro Prime nubmer
+
